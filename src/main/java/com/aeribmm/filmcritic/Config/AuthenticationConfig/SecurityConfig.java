c9c2c6bab98ad8a2093589fc7118f037af71fa31
aeribmm <maksym.fisiak.work@gmail.com>
2025-05-26 03:21:38 +0200

    maybe win

--- src/main/java/com/aeribmm/filmcritic/Config/AuthenticationConfig/SecurityConfig.java
+++ src/main/java/com/aeribmm/filmcritic/Config/AuthenticationConfig/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
 @EnableWebSecurity
@@ -40,6 +42,7 @@ public class SecurityConfig {
 
 
         http
+                .cors(withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("auth",
